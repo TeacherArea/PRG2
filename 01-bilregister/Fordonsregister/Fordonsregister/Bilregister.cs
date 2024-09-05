c0e4d636ee0f3bd0aaa17e892cb90ac3dcfddf30
TeacherArea <sewilius.berg@initialize.se>
2024-09-05 09:07:57 +0200

    Uppdaterad med exempel på egenskapsvalidering

--- 01-bilregister/Fordonsregister/Fordonsregister/Bilregister.cs
+++ 01-bilregister/Fordonsregister/Fordonsregister/Bilregister.cs
@@ -12,8 +12,8 @@ namespace Fordonsregister
 {
     public partial class Form_Main : Form
     {
-        List<Fordon> fordon = new List<Fordon>();
-        
+        List<Fordon> fordonLista = new List<Fordon>();
+
         public Form_Main()
         {
             InitializeComponent();
@@ -23,34 +23,68 @@ namespace Fordonsregister
         }
         private void Btn_Registrera_Click(object sender, EventArgs e)
         {
-            string regNr = Fordon.GodkännRegNr(txtBoxIn_RegNr.Text);
-            if (regNr == null)
-            {
-                MessageBox.Show("Vänligen använd formatet ABC123", Text);
-                return;
-            }
-
-            foreach (Fordon f in fordon)
-            {
-                if (regNr == f.RegNr)
-                {
-                    MessageBox.Show("Registreingsnumret finns redan inlagt i registret", Text); 
-                }
-            }
-            Fordon nyttFordon = new Fordon(regNr, txtBoxIn_Märke.Text, txtBoxIn_Modell.Text, 
+            try
+            {   
+                Fordon nyttFordon = new Fordon("", txtBoxIn_Märke.Text, txtBoxIn_Modell.Text,
                 (Fordon.Typ)dropDownList_Typ.SelectedIndex);
 
-            fordon.Add(nyttFordon);
-            listBox_Register.Items.Add(nyttFordon);
-            txtBoxIn_RegNr.Clear();
-            txtBoxIn_Märke.Clear();
-            txtBoxIn_Modell.Clear();
+                string regNr = txtBoxIn_RegNr.Text;
+                nyttFordon.RegNr = regNr;
+
+                fordonLista.Add(nyttFordon);
+                listBox_Register.Items.Add(nyttFordon);
+                txtBoxIn_RegNr.Clear();
+                txtBoxIn_Märke.Clear();
+                txtBoxIn_Modell.Clear();
+            }
+
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, Text);
+            }
+
+
+
+            /* Om man använder en metod istället för egenskaper, kan denna kod fungera istället
+             * lägg märket till att denna kod använder den andra konstruktorn
+             */
+
+            //try
+            //{
+            //    string regNr = Fordon.GodkännRegNr(txtBoxIn_RegNr.Text);
+
+            //    if (regNr == null)
+            //    {
+            //        MessageBox.Show("Vänligen använd formatet ABC123", Text);
+            //        return;
+            //    }
+
+            //    foreach (Fordon f in fordonLista)
+            //    {
+            //        if (regNr == f.RegNr)
+            //        {
+            //            MessageBox.Show("Registreringsnumret finns redan inlagt i registret", Text);
+            //        }
+            //    }
+            //    Fordon nyttFordon = new Fordon(regNr, txtBoxIn_Märke.Text, txtBoxIn_Modell.Text,
+            //        (Fordon.Typ)dropDownList_Typ.SelectedIndex);
+
+            //    fordonLista.Add(nyttFordon);
+            //    listBox_Register.Items.Add(nyttFordon);
+            //    txtBoxIn_RegNr.Clear();
+            //    txtBoxIn_Märke.Clear();
+            //    txtBoxIn_Modell.Clear();
+            //}
+            //catch
+            //{
+            //    MessageBox.Show("Något gick fel", Text);
+            //}
         }
 
         private void radioBtn_CheckedChanged(object sender, EventArgs e)
         {
             listBox_Register.Items.Clear();
-            foreach (Fordon f in fordon)
+            foreach (Fordon f in fordonLista)
             {
                 if (f.Fordonstyp != Fordon.Typ.Bil && radioBtn_Bilar.Checked) continue;
                 if (f.Fordonstyp != Fordon.Typ.MC && radioBtn_MC.Checked) continue;
--- 01-bilregister/Fordonsregister/Fordonsregister/Fordon.cs
+++ 01-bilregister/Fordonsregister/Fordonsregister/Fordon.cs
@@ -15,8 +15,9 @@ namespace Fordonsregister
         private string modell;
         private Typ fordonstyp;
 
+
         //Konstruktor (ett slag av klassmetod, enbart gjord för klasser)
-        public Fordon(string regnr, string märke, string modell, Typ fordonstyp)
+        public Fordon(string regnr, string märke, string modell, Typ fordonstyp) // en konstruktor som tar argument
         {
             this.regNr = regnr;
             this.märke = märke;
@@ -28,7 +29,41 @@ namespace Fordonsregister
         public string RegNr
         {
             get { return regNr; }
-            set { this.regNr = value; }
+
+            // set { this.regNr = value; } //Används istället GodkännRegNr() kan det räcka med ett enbart returnerande värde.
+            // Testa, men glöm inte kommentera bort den andra set
+
+            set
+            {
+                if (value.Length == 6)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (!char.IsLetter(value[i]))
+                            throw new ArgumentException("Inkorret registreringsnummer: De första tre tecknen måste vara bokstäver.");
+                    }
+
+                    for (int i = 3; i < 6; i++)
+                    {
+                        if (i < 5)
+                        {
+                            if (!char.IsDigit(value[i]))
+                                throw new ArgumentException("Inkorret registreringsnummer: Det fjärde och femte tecknet måste vara siffror.");
+                        }
+                        else
+                        {
+                            if (!char.IsDigit(value[i]) && !char.IsLetter(value[i]))
+                                throw new ArgumentException("Inkorret registreringsnummer: Det sjätte tecknet måste vara en siffra eller en bokstav.");
+                        }
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Ett registreringsnummer måste bestå av exakt 6 tecken, med tre bokstäver följt av två siffror och en siffra eller bokstav.");
+                }
+
+                regNr = value.ToUpper();
+            }
         }
 
         public string Märke
@@ -49,7 +84,7 @@ namespace Fordonsregister
             set { this.fordonstyp = value; }
         }
 
-        //Klassens övriga (vaniiga metoder)
+        //Klassens övriga metoder, här bestående av en annan lösning för att testa RegNr, och en override av ToString()
         public static string GodkännRegNr(string regNr)
         {
             if (regNr.Length == 6)
